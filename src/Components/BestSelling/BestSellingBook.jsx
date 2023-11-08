ec8af08928ba8e8d046c5df2b4ded5b8e0c89db1
mostofa kamal <mostofaoffical.net24@gmail.com>
2023-11-09 01:45:23 +0600

    selling book issue fixed

--- src/Components/BestSelling/BestSellingBook.jsx
+++ src/Components/BestSelling/BestSellingBook.jsx
@@ -2,31 +2,37 @@ import { useEffect, useState } from "react";
 import SellingBookDetails from "./SellingBookDetails";
 
 const BestSellingBook = () => {
-  const [allCart, setAllCart] = useState([]);
-  const [showAll, setShowAll] = useState(false);
+    const [allCart, setAllCart] = useState([]);
+    const [showAll, setShowAll] = useState(false);
 
-  useEffect(() => {
-    fetch('bestSellBook.json')
-      .then(res => res.json())
-      .then(data => setAllCart(data))
-  }, []);
+    useEffect(() => {
+        fetch('bestSellBook.json')
+            .then(res => res.json())
+            .then(data => setAllCart(data))
+    }, []);
 
-  const handleShowAllCart = () => {
-    setShowAll(true);
-  }
+    const handleShowAllCart = () => {
+        setShowAll(true);
+    }
 
-  const displayedCards = showAll ? allCart : allCart.slice(0, 3);
+    const displayedCards = showAll ? allCart : allCart.slice(0, 3);
 
-  return (
-    <div className="grid md:grid-cols-3 gap-2 ">
-      {displayedCards.map(item => <SellingBookDetails key={item.id} item={item} />)}
-      {!showAll && (
-        <div className="text-center">
-          <button onClick={handleShowAllCart} className="btn">Show All</button>
+    return (
+        <div>
+            <div className="flex justify-between">
+                <h1 className="text-4xl text-purple-300 ">Best Selling Book</h1>
+                {!showAll && (
+                    <div className="text-center">
+                        <button onClick={handleShowAllCart} className="btn btn-secondary">Show All</button>
+                    </div>
+                )}
+            </div>
+            <div className="grid md:grid-cols-3 gap-2 ">
+                {displayedCards.map(item => <SellingBookDetails key={item.id} item={item} />)}
+
+            </div>
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default BestSellingBook;
